fc5c909d9db730ed2355b15b4546c3d78e0d96d8
Damian R. Molin <119323521+DamianPyCoder@users.noreply.github.com>
2024-04-02 23:34:41 +0200

    Add files via upload

---
+++ 09_python_youtuber/contarPalabras.py
@@ -0,0 +1,4 @@
+texto = "Hola, esto es una cadena de texto de ejemplo"
+palabras = texto.split()
+cantidad_palabras = len(palabras)
+print("Cantidad de palabras en el texto:", cantidad_palabras)  # Resultado: 9
